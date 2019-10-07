fb3f67fbea0ecc31ae297cdc7840915a54b12670
Miguel Sepulcre <msepulcre@umh.es>
2019-10-07 16:59:18 +0200

    Update paper reference

--- CV2XMode4_common.m
+++ CV2XMode4_common.m
@@ -6,9 +6,11 @@ function [ deltaHD , deltaSEN , deltaPRO ] = CV2XMode4_common( lambda , Pt , dis
 % probability of packet loss due to propagation effects for different 
 % Tx-Rx distances based on the models described in the following paper:
 % 
-%    Manuel Gonzalez-Martín, Miguel Sepulcre, Rafael Molina-Masegosa, Javier Gozalvez, 
+%    Manuel Gonzalez-MartÃ­n, Miguel Sepulcre, Rafael Molina-Masegosa, Javier Gozalvez, 
 %    "Analytical Models of the Performance of C-V2X Mode 4 Vehicular Communications", 
-%    arXiv:1807.06508 [cs.NI], Submitted on 17 July 2018, https://arxiv.org/abs/1807.06508
+%    IEEE Transactions on Vehicular Technology, Vol. 68, Issue 2, Feb. 2019. DOI: 10.1109/TVT.2018.2888704
+%    Final version available at: https://ieeexplore.ieee.org/document/8581518
+%    Post-print version available at: https://arxiv.org/abs/1807.06508
 %
 % CV2XMode4_common is called from the main script, CV2XMode4.
 %
