9ab47b0b7ba498e286af5326c1127d9e489c1f51
Miguel Sepulcre <msepulcre@umh.es>
2019-10-07 16:59:43 +0200

    Update paper reference

--- get_BLER.m
+++ get_BLER.m
@@ -7,9 +7,11 @@ function [ avg_BLER ] = get_BLER ( SINR , PDF , coding , step_dB)
 % CV2XMode4_Step2 and CV2XMode4_Step3 to model the communication performance 
 % of C-V2X or LTE-V Mode 4 using the analytical models described in:
 % 
-%    Manuel Gonzalez-Martín, Miguel Sepulcre, Rafael Molina-Masegosa, Javier Gozalvez, 
+%    Manuel Gonzalez-MartÃ­n, Miguel Sepulcre, Rafael Molina-Masegosa, Javier Gozalvez, 
 %    "Analytical Models of the Performance of C-V2X Mode 4 Vehicular Communications", 
-%    arXiv:1807.06508 [cs.NI], Submitted on 17 July 2018, https://arxiv.org/abs/1807.06508
+%    IEEE Transactions on Vehicular Technology, Vol. 68, Issue 2, Feb. 2019. DOI: 10.1109/TVT.2018.2888704
+%    Final version available at: https://ieeexplore.ieee.org/document/8581518
+%    Post-print version available at: https://arxiv.org/abs/1807.06508
 %
 % Input parameters:
 %    SINR: Signal-to-Interference and Noise Ratio levels for different distances. It is a matrix where each row represent the SINR levels for a given Tx-Rx distance
