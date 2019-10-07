194fa9b7bf1185ba2cfb827a8df50413b7809e25
Miguel Sepulcre <msepulcre@umh.es>
2019-10-07 16:58:54 +0200

    Update paper reference

--- CV2XMode4_Step3.m
+++ CV2XMode4_Step3.m
@@ -4,9 +4,11 @@ function [ deltaCOL ] = CV2XMode4_Step3( beta , lambda , Pt , S , distance , Pse
 % considering only Step 3 of C-V2X or LTE-V Mode 4 and the CBR based on the
 % models described in the following paper:
 % 
-%    Manuel Gonzalez-Martín, Miguel Sepulcre, Rafael Molina-Masegosa, Javier Gozalvez, 
+%    Manuel Gonzalez-MartÃ­n, Miguel Sepulcre, Rafael Molina-Masegosa, Javier Gozalvez, 
 %    "Analytical Models of the Performance of C-V2X Mode 4 Vehicular Communications", 
-%    arXiv:1807.06508 [cs.NI], Submitted on 17 July 2018, https://arxiv.org/abs/1807.06508
+%    IEEE Transactions on Vehicular Technology, Vol. 68, Issue 2, Feb. 2019. DOI: 10.1109/TVT.2018.2888704
+%    Final version available at: https://ieeexplore.ieee.org/document/8581518
+%    Post-print version available at: https://arxiv.org/abs/1807.06508
 %
 % CV2XMode4_Step3 is called from the main script, CV2XMode4.
 %
